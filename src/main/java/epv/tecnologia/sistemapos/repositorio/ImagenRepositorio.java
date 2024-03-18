a8d2f5f70ba57457d1e535579070c2e5cc31a0a9
edwinparrales <edwinparrales82@gmail.com>
2024-03-17 19:04:53 -0500

    Primer commit

--- src/main/java/epv/tecnologia/sistemapos/repositorio/ImagenRepositorio.java
+++ src/main/java/epv/tecnologia/sistemapos/repositorio/ImagenRepositorio.java
@@ -1,7 +1,15 @@
 package epv.tecnologia.sistemapos.repositorio;
 
 import epv.tecnologia.sistemapos.entidad.Imagen;
+import epv.tecnologia.sistemapos.entidad.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 
 public interface ImagenRepositorio extends JpaRepository<Imagen,String> {
+    @Modifying
+    @Query("update Imagen i set i.url = :url, i.producto = :producto where upper(i.id) like upper(:id)")
+    void actualizar(@NonNull @Param("url") String url,@Param("producto") Producto producto, @NonNull @Param("id") String id);
 }
--- src/main/java/epv/tecnologia/sistemapos/servicio/ImagenSrv.java
+++ src/main/java/epv/tecnologia/sistemapos/servicio/ImagenSrv.java
@@ -1,33 +1,41 @@
 package epv.tecnologia.sistemapos.servicio;
 
 import epv.tecnologia.sistemapos.entidad.Imagen;
+import epv.tecnologia.sistemapos.repositorio.ImagenRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ImagenSrv implements ServicioGenerico<Imagen,String>{
+    @Autowired
+    private ImagenRepositorio imgRep;
     @Override
     public Imagen guardar(Imagen imagen) {
-        return null;
+
+        return imgRep.save(imagen);
     }
 
     @Override
     public List<Imagen> listar() {
-        return null;
+
+        return imgRep.findAll();
     }
 
     @Override
     public List<Imagen> listar(String parametro) {
-        return null;
+
+        return (List<Imagen>) imgRep.findById(parametro).get();
     }
 
     @Override
     public void eliminar(String id) {
-
+       imgRep.deleteById(id);
     }
 
     @Override
-    public void actualizar(Imagen object) {
-
+    public void actualizar(Imagen imagen) {
+        imgRep.actualizar(imagen.getUrl(),imagen.getProducto(), imagen.getId());
     }
 
 
